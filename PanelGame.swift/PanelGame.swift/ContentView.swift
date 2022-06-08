9cf618ef3a31f9985d8325052965b9654ed0e2ae
jin-kirino <jinkirino@gmail.com>
2022-06-08 22:02:15 +0900

    first commit

---
+++ PanelGame.swift/PanelGame.swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PanelGame.swift
+//
+//  Created by 神　樹里乃 on 2022/06/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PanelGame.swift/PanelGame.swift/PanelGame_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PanelGame_swiftApp.swift
+//  PanelGame.swift
+//
+//  Created by 神　樹里乃 on 2022/06/08.
+//
+
+import SwiftUI
+
+@main
+struct PanelGame_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
