a41a51dc28e032942a661e2d2d08bc466b654118
jin-kirino <jinkirino@gmail.com>
2022-06-10 07:36:20 +0900

    NavigationViewを追加

--- PanelGame/PanelGame/ContentView.swift
+++ PanelGame/PanelGame/ContentView.swift
@@ -12,38 +12,32 @@ struct ContentView: View {
     @State private var columns: [GridItem] = Array(repeating: .init(.fixed(120)), count: 3)
 
     var body: some View {
-        ZStack {
-            // 画面いっぱい真っ黒
-            Color.black
-                .ignoresSafeArea()
-            VStack {
-                Text("パネルゲーム")
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-                    .fontWeight(.medium)
-                    // 左上寄せ
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.bottom, 130)
-                ScrollView(.vertical) {
+
+        NavigationView {
+            ZStack {
+                // 画面いっぱい真っ黒
+                Color.gray
+                    .ignoresSafeArea()
                     // 幅が指定通りで高さがView要素に応じて広がる
                     // (colums: [GridItem]配列を渡す, alignment: 行の配置を.center .top .bottom で指定, spacing: 行の間隔)
                     LazyVGrid(columns: columns, alignment: .center, spacing: 15) {
-                        // 分かりやすいようにナンバリング
                         ForEach((0...8), id: \.self) { number in
                             ZStack {
                                 // 円の半径
                                 RoundedRectangle(cornerRadius: 10)
-                                    // 塗りつぶしの色
+                                // 塗りつぶしの色
                                     .fill(Color.white)
-                                    // フレームのサイズ指定
+                                // フレームのサイズ指定
                                     .frame(width: 115, height: 115)
+                                // 分かりやすいようにナンバリング
                                 Text("\(number)")
+                                    .foregroundColor(.blue)
                             }// ZStack
                         }// ForEach
                     }// LazyVGrid
-                }// ScrollView
-            }// VStack
-        }// ZStack
+            }// ZStack
+            .navigationTitle("パネルゲーム").foregroundColor(.white)
+        }// NavigationView
     }// body
 }// ContentView
 
@@ -52,3 +46,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+// タイトルの文字色を白にするには？
