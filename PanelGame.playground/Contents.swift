2d1cbe544d92b342303b222b1ae36a7eff3153f7
jin-kirino <jinkirino@gmail.com>
2022-06-19 15:59:44 +0900

    横方向を文字を使って確認

--- PanelGame.playground/Contents.swift
+++ PanelGame.playground/Contents.swift
@@ -3,25 +3,51 @@ import UIKit
 
 var moves: [String] = Array(repeating: "", count: 9)
 
-private func checkPanel(_ panels: [String], player: String) -> Bool {
+private func checkPanels(_ panels: [String], player: String) -> Bool {
     // 横
-    if panels[0] == player && panels[1] == player && panels[2] == player {
-        return true
-    } else if panels[0] == player && panels[1] == player && panels[2] == player {
-        return true
-    } else if panels[3] == player && panels[4] == player && panels[5] == player {
-        return true
-    } else if panels[6] == player && panels[7] == player && panels[8] == player {
-        return true
+    let yoko1 = 0
+    let yoko2 = 3
+    let yoko3 = 6
+//
+    
+    
+    for number in 0...2 {
+        print("number1:\(number)")
+        if panels[yoko1 + number] == player {
+            print("yoko + number:\(yoko1 + number)")
+            return true
+        } else if panels[yoko2 + number] == player {
+                return true
+        } else if panels[yoko3 + number] == player {
+            return true
+        }
+    }
+    
+//    if panels[yoko1] == player && panels[yoko1 + 1] == player && panels[yoko1 + 2] == player {
+//        return true
+//    } else if panels[yoko2] == player && panels[yoko2 + 1] == player && panels[yoko2 + 2] == player {
+//        return true
+//    } else if panels[yoko3] == player && panels[yoko3 + 1] == player && panels[yoko3 + 2] == player {
+//        return true
+    
+//    }
+
+    
+    
     // 縦
-    } else if panels[0] == player && panels[3] == player && panels[6] == player {
+    let tate0 = 0
+    let tate1 = 1
+    let tate2 = 2
+    if panels[tate0] == player && panels[tate0 + 3] == player && panels[tate0 + 6] == player {
         return true
-    } else if panels[1] == player && panels[4] == player && panels[7] == player {
+    } else if panels[tate1] == player && panels[tate1 + 3] == player && panels[tate1 + 6] == player {
         return true
-    } else if panels[2] == player && panels[5] == player && panels[8] == player {
+    } else if panels[tate2] == player && panels[tate2 + 3] == player && panels[tate2 + 6] == player {
         return true
+    }
+    
     // 斜め
-    } else if panels[0] == player && panels[4] == player && panels[8] == player {
+    if panels[0] == player && panels[4] == player && panels[8] == player {
         return true
     } else if panels[2] == player && panels[4] == player && panels[6] == player {
         return true
@@ -39,5 +65,11 @@ moves[3] = "🐱"
 moves[5] = "🐱"
 print(moves)
 
-let ccc = checkPanel(moves, player: "🐶")
-let ddd = checkPanel(moves, player: "🐱")
+let ccc = checkPanels(moves, player: "🐶")
+let ddd = checkPanels(moves, player: "🐱")
+let eee = checkPanels(moves, player: "🐶")
+let fff = checkPanels(moves, player: "🐱")
+
+// 1.2.3を一つの変数にまとめるためにfor stride
+// まとめた変数を
+
