01944335f7b5b957997be5372b258effd123ca90
jin-kirino <jinkirino@gmail.com>
2022-06-22 12:31:39 +0900

    縦のチェックをfor文で指定

--- PanelGame/PanelGame/ContentView.swift
+++ PanelGame/PanelGame/ContentView.swift
@@ -72,9 +72,9 @@ struct ContentView: View {
 //        let yoko1 = 0
 //        let yoko2 = 3
 //        let yoko3 = 6
-        let tate0 = 0
-        let tate1 = 1
-        let tate2 = 2
+//        let tate0 = 0
+//        let tate1 = 1
+//        let tate2 = 2
 
 //        // 0,1,2
 //        for number in 0...2 {
@@ -155,13 +155,7 @@ struct ContentView: View {
 //                return false
 //            }
 //        }
-        for number in stride(from: 0, through: 8, by: 3) {
-            if panels[number] == player && panels[number + 1] == player && panels[number + 2] == player {
-                return true
-            } else {
-                return false
-            }
-        }
+
 //        if panels[yoko1] == player && panels[yoko1 + 1] == player && panels[yoko1 + 2] == player {
 //                return true
 //        } else if panels[yoko2] == player && panels[yoko2 + 1] == player && panels[yoko2 + 2] == player {
@@ -169,15 +163,36 @@ struct ContentView: View {
 //        } else if panels[yoko3] == player && panels[yoko3 + 1] == player && panels[yoko3 + 2] == player {
 //                return true
 //        } else
-        // 縦
-        if panels[tate0] == player && panels[tate0 + 3] == player && panels[tate0 + 6] == player {
-            return true
-        } else if panels[tate1] == player && panels[tate1 + 3] == player && panels[tate1 + 6] == player {
-            return true
-        } else if panels[tate2] == player && panels[tate2 + 3] == player && panels[tate2 + 6] == player {
-            return true
+        for number in stride(from: 0, through: 8, by: 3) {
+            if panels[number] == player && panels[number + 1] == player && panels[number + 2] == player {
+                return true
+            }
+        }
+//        // 縦
+//        if panels[tate0] == player && panels[tate0 + 3] == player && panels[tate0 + 6] == player {
+//            return true
+//        } else if panels[tate1] == player && panels[tate1 + 3] == player && panels[tate1 + 6] == player {
+//            return true
+//        } else if panels[tate2] == player && panels[tate2 + 3] == player && panels[tate2 + 6] == player {
+//            return true
+//
+            for number in 0...2 {
+                if panels[number] == player && panels[number + 3] == player && panels[number + 6] == player {
+                    return true
+                }
+            }
         // 斜め
-        } else if panels[0] == player && panels[4] == player && panels[8] == player {
+//        for number in stride(from: 0, through: 8, by: 4) {
+//            if panels[number] == player && panels[number] == player && panels[number] == player {
+//                return true
+//            }
+//        }
+//        for number in stride(from: 2, through: 6, by: 2) {
+//            if panels[number] == player && panels[number] == player && panels[number] == player {
+//                return true
+//            }
+//        }
+        if panels[0] == player && panels[4] == player && panels[8] == player {
             return true
         } else if panels[2] == player && panels[4] == player && panels[6] == player {
             return true
